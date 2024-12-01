1a0be7832b7cdfb3e973aca343ca6f9f72ade72e
Mykola Balan <balann705@gmail.com>
2024-12-01 21:35:23 +0200

    finish task2

--- LeetCode_First/src/solution/Task.java
+++ LeetCode_First/src/solution/Task.java
@@ -28,17 +28,8 @@ public class Task {
         return uniqueValue;
     }
 
-    public int removeElement(int[] nums, int val) {
 
-        int counter = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i]==val){
-                counter++;
 
-            }
-        }
 
-    return  counter;
-    }
 }
